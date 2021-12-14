cff3ecea5f8831903efcc74ad5e7be2e0344320d
ThatOneAussie <lincolnellwood@outlook.com>
2021-12-14 17:28:47 +1000

    started working on topaz
    added 2d texture for topaz armour
    added topaz
    i think thats it

---
+++ src/main/java/net/mcreator/aussieshit/block/TopazOreBlock.java
@@ -0,0 +1,58 @@
+
+package net.mcreator.aussieshit.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.aussieshit.itemgroup.AusshittabItemGroup;
+import net.mcreator.aussieshit.item.TopazItem;
+import net.mcreator.aussieshit.AussieShitModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@AussieShitModElements.ModElement.Tag
+public class TopazOreBlock extends AussieShitModElements.ModElement {
+	@ObjectHolder("aussie_shit:topaz_ore")
+	public static final Block block = null;
+	public TopazOreBlock(AussieShitModElements instance) {
+		super(instance, 66);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(AusshittabItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.STONE).hardnessAndResistance(3.5f, 10f).setLightLevel(s -> 0).harvestLevel(2)
+					.harvestTool(ToolType.PICKAXE).setRequiresTool());
+			setRegistryName("topaz_ore");
+		}
+
+		@Override
+		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return 15;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(TopazItem.block));
+		}
+	}
+}

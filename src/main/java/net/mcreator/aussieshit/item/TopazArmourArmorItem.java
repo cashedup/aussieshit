8e24a1812eb6891a3551517f2182f49544ecc4d6
ThatOneAussie <lincolnellwood@outlook.com>
2021-12-14 17:43:57 +1000

    topaz updates
    generated topaz tools
    topaz armour recipes

---
+++ src/main/java/net/mcreator/aussieshit/item/TopazArmourArmorItem.java
@@ -0,0 +1,104 @@
+
+package net.mcreator.aussieshit.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.aussieshit.AussieShitModElements;
+
+@AussieShitModElements.ModElement.Tag
+public class TopazArmourArmorItem extends AussieShitModElements.ModElement {
+	@ObjectHolder("aussie_shit:topaz_armour_armor_helmet")
+	public static final Item helmet = null;
+	@ObjectHolder("aussie_shit:topaz_armour_armor_chestplate")
+	public static final Item body = null;
+	@ObjectHolder("aussie_shit:topaz_armour_armor_leggings")
+	public static final Item legs = null;
+	@ObjectHolder("aussie_shit:topaz_armour_armor_boots")
+	public static final Item boots = null;
+	public TopazArmourArmorItem(AussieShitModElements instance) {
+		super(instance, 82);
+	}
+
+	@Override
+	public void initElements() {
+		IArmorMaterial armormaterial = new IArmorMaterial() {
+			@Override
+			public int getDurability(EquipmentSlotType slot) {
+				return new int[]{13, 15, 16, 11}[slot.getIndex()] * 15;
+			}
+
+			@Override
+			public int getDamageReductionAmount(EquipmentSlotType slot) {
+				return new int[]{2, 6, 5, 2}[slot.getIndex()];
+			}
+
+			@Override
+			public int getEnchantability() {
+				return 9;
+			}
+
+			@Override
+			public net.minecraft.util.SoundEvent getSoundEvent() {
+				return (net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation(""));
+			}
+
+			@Override
+			public Ingredient getRepairMaterial() {
+				return Ingredient.fromStacks(new ItemStack(TopazItem.block));
+			}
+
+			@OnlyIn(Dist.CLIENT)
+			@Override
+			public String getName() {
+				return "topaz_armour_armor";
+			}
+
+			@Override
+			public float getToughness() {
+				return 0f;
+			}
+
+			@Override
+			public float getKnockbackResistance() {
+				return 0f;
+			}
+		};
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.HEAD, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "aussie_shit:textures/models/armor/topazarmour_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("topaz_armour_armor_helmet"));
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.CHEST, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "aussie_shit:textures/models/armor/topazarmour_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("topaz_armour_armor_chestplate"));
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.LEGS, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "aussie_shit:textures/models/armor/topazarmour_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("topaz_armour_armor_leggings"));
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.FEET, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "aussie_shit:textures/models/armor/topazarmour_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("topaz_armour_armor_boots"));
+	}
+}
---
+++ src/main/java/net/mcreator/aussieshit/item/TopazToolsSwordItem.java
@@ -0,0 +1,52 @@
+
+package net.mcreator.aussieshit.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import net.mcreator.aussieshit.AussieShitModElements;
+
+@AussieShitModElements.ModElement.Tag
+public class TopazToolsSwordItem extends AussieShitModElements.ModElement {
+	@ObjectHolder("aussie_shit:topaz_tools_sword")
+	public static final Item block = null;
+	public TopazToolsSwordItem(AussieShitModElements instance) {
+		super(instance, 74);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new SwordItem(new IItemTier() {
+			public int getMaxUses() {
+				return 250;
+			}
+
+			public float getEfficiency() {
+				return 6f;
+			}
+
+			public float getAttackDamage() {
+				return 0f;
+			}
+
+			public int getHarvestLevel() {
+				return 2;
+			}
+
+			public int getEnchantability() {
+				return 14;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.fromStacks(new ItemStack(TopazItem.block));
+			}
+		}, 3, -3f, new Item.Properties().group(ItemGroup.COMBAT)) {
+		}.setRegistryName("topaz_tools_sword"));
+	}
+}
